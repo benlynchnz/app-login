4cd890eb6288eb5bfb3ec9f63525aaa30b8b849f
Ben Lynch <ben.lynch@eroad.com>
2015-10-21 15:51:25 +1300

    version

--- gulpfile.js
+++ gulpfile.js
@@ -7,37 +7,68 @@ var path = require("path");
 var s3 = require("gulp-awspublish");
 var config = require("./package.json");
 var rename = require("gulp-rename");
+var aws = require("aws-sdk");
+var s3Fetch = new aws.S3({
+  accessKeyId: process.env.AWS_ACCESS_KEY,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  region: process.env.AWS_REGION
+});
+
+function getVersion(callback) {
+  var params = {
+    Bucket: process.env.AWS_BUCKET,
+    Prefix: config.s3URI
+  };
+
+  s3Fetch.listObjects(params, function(err, data) {
+    if (err) {
+      console.warn(err);
+    }
+    var last = data.Contents.sort(function(item) {
+      return item.LastModified;
+    });
+
+    var version = last[0].Key.replace(config.s3URI, "").replace("/", "");
+    var int = Number(version.replace("/", ""));
+    int = int + 1;
+    callback(null, int);
+  });
+}
 
 gulp.task("deploy", function() {
 
-	var publisher = s3.create({
-		accessKeyId: process.env.AWS_ACCESS_KEY,
-		secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-		region: process.env.AWS_REGION,
-		params: {
-			Bucket: process.env.AWS_BUCKET
-		}
-	});
+	getVersion(function(err, version) {
 
-	var headers = {
-    "Cache-Control": "max-age=0, no-transform, public",
-		"Metadata" : {
-			"commit": process.env.CI_COMMIT_ID,
-			"build-url": process.env.CI_BUILD_URL,
-			"build-number": process.env.CI_BUILD_NUMBER,
-			"committer-name": process.env.CI_COMMITTER_NAME,
-			"committer-email": process.env.CI_COMMITTER_EMAIL,
-			"committer-username": process.env.CI_COMMITTER_USERNAME,
-			"message": process.env.CI_MESSAGE
-		}
+  var publisher = s3.create({
+    accessKeyId: process.env.AWS_ACCESS_KEY,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    region: process.env.AWS_REGION,
+    params: {
+      Bucket: process.env.AWS_BUCKET
+    }
+  });
+
+  var headers = {
+    "Cache-Control": "max-age=99999999, no-transform, public",
+    "Metadata" : {
+      "commit": process.env.CI_COMMIT_ID || null,
+      "build-url": process.env.CI_BUILD_URL || null,
+      "build-number": process.env.CI_BUILD_NUMBER || null,
+      "committer-name": process.env.CI_COMMITTER_NAME || null,
+      "committer-email": process.env.CI_COMMITTER_EMAIL || null,
+      "committer-username": process.env.CI_COMMITTER_USERNAME || null,
+      "message": process.env.CI_MESSAGE || null
+    }
   };
 
-  return gulp.src(["./dist/*"])
-		.pipe(rename(function(path) {
-        path.dirname += "/" + config.s3URI;
+  return gulp.src([ "./dist/*" ])
+    .pipe(rename(function(path) {
+      path.dirname += "/" + config.s3URI + "/" + version;
     }))
     .pipe(publisher.publish(headers))
     .pipe(s3.reporter());
+
+	});
 });
 
 gulp.task("html", function() {
@@ -67,7 +98,7 @@ gulp.task("watch", [ "server" ], function() {
   gulp.watch("./src/**/*.js", [ "webpack" ]);
   gulp.watch("./src/**/*.css", [ "webpack" ]);
   gulp.watch("./src/**/*.html", [ "html" ]);
-	// gulp.watch("./dist/*", ["deploy"]);
+  // gulp.watch("./dist/*", ["deploy"]);
 });
 
 gulp.task("default", function() {
