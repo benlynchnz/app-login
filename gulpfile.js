08a6e6bb9b5c0ade641479d31d5792f22ced0d44
Ben Lynch <ben.lynch@eroad.com>
2015-10-22 10:37:24 +1300

    test

--- gulpfile.js
+++ gulpfile.js
@@ -48,51 +48,56 @@ function getVersion(callback) {
         });
       });
     } else {
-	    console.log(data);
+      console.log(data);
 
-			var file = new Buffer(data.Body).toString();
-			file = JSON.parse(file);
+      var file = new Buffer(data.Body).toString();
+      file = JSON.parse(file);
 
-			var version = file.version + 1;
-			callback(null, version);
-		}
+      var version = file.version + 1;
+      callback(null, version);
+    }
   });
 }
 
 gulp.task("deploy", function() {
 
   getVersion(function(err, version) {
+    var file = "./dist/build.json";
+    var obj = {
+      version: version,
+      created_at: moment().toISOString(),
+      updated_at: moment().toISOString(),
+      commit: process.env.CI_COMMIT_ID || null,
+      build_uri: process.env.CI_BUILD_URL || null,
+      build_number: process.env.CI_BUILD_NUMBER || null,
+      committer_name: process.env.CI_COMMITTER_NAME || null,
+      committer_email: process.env.CI_COMMITTER_EMAIL || null,
+      committer_username: process.env.CI_COMMITTER_USERNAME || null,
+      message: process.env.CI_MESSAGE || null
+    };
+    jsonfile.writeFile(file, obj, { spaces: 2 }, function(error) {
+      var publisher = s3.create({
+        accessKeyId: process.env.AWS_ACCESS_KEY,
+        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+        region: process.env.AWS_REGION,
+        params: {
+          Bucket: process.env.AWS_BUCKET
+        }
+      });
 
-  var publisher = s3.create({
-    accessKeyId: process.env.AWS_ACCESS_KEY,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-    region: process.env.AWS_REGION,
-    params: {
-      Bucket: process.env.AWS_BUCKET
-    }
-  });
+      var headers = {
+        "Cache-Control": "max-age=99999999, no-transform, public"
+      };
 
-  var headers = {
-    "Cache-Control": "max-age=99999999, no-transform, public",
-    "Metadata" : {
-      "commit": process.env.CI_COMMIT_ID || null,
-      "build-url": process.env.CI_BUILD_URL || null,
-      "build-number": process.env.CI_BUILD_NUMBER || null,
-      "committer-name": process.env.CI_COMMITTER_NAME || null,
-      "committer-email": process.env.CI_COMMITTER_EMAIL || null,
-      "committer-username": process.env.CI_COMMITTER_USERNAME || null,
-      "message": process.env.CI_MESSAGE || null
-    }
-  };
+      return gulp.src([ "./dist/*" ])
+        .pipe(rename(function(path) {
+          path.dirname += "/" + config.s3URI + "/" + version;
+        }))
+        .pipe(publisher.publish(headers))
+        .pipe(s3.reporter());
+      });
+    });
 
-  return gulp.src([ "./dist/*" ])
-    .pipe(rename(function(path) {
-      path.dirname += "/" + config.s3URI + "/" + version;
-    }))
-    .pipe(publisher.publish(headers))
-    .pipe(s3.reporter());
-
-  });
 });
 
 gulp.task("html", function() {
