3b16d08119a5cf8bed6d77a267919d8743e6dd46
Ben Lynch <ben.lynch@eroad.com>
2015-10-23 10:53:47 +1300

    update

--- src/Content.jsx
+++ src/Content.jsx
@@ -1,22 +1,29 @@
 import css from "./Login.css";
 
 export default class LoginAppContent extends React.Component {
-  static propTypes = {
-    //element: React.PropTypes.object.isRequired
-  }
-
-  static defaultProps = {
-    //element: {}
-  }
-
   constructor(props) {
     super(props);
+    this._onClick = this._onClick.bind(this);
+  }
+
+  _onClick() {
+    const pass = this.refs.password.getDOMNode().value;
+    if (pass === "letmesee") {
+      window.location = "/builds";
+    } else {
+      alert("nope");
+    }
   }
 
   render() {
     return (
       <div className={css.content}>
-        <p>A login form</p>
+        <input
+          type="password"
+          ref="password"
+          className={css.input}
+          placeholder="password" />
+        <button className={css.button} onClick={this._onClick}>Go</button>
       </div>
     );
   }
